5aa7ed679ac54c98e9c8abead2bb425f483c897d
Jorge Rodriguez <bs.vino@gmail.com>
2014-07-17 05:27:55 +0000

    Many improvements to the leaderboard.

--- leaderboards/leaderboards.py
+++ leaderboards/leaderboards.py
@@ -54,13 +54,13 @@ def calc_leaders(database, leaderboards, output):
 
 	# Initialize all players to empty lists, just so we have the keys, to avoid repeated lookups of players in multiple lists.
 	for player in daily_leaders.leaders:
-		players_by_id[player] = []
+		players_by_id[player] = database_pb2.players_entry()
 
 	for player in weekly_leaders.leaders:
-		players_by_id[player] = []
+		players_by_id[player] = database_pb2.players_entry()
 
 	for player in monthly_leaders.leaders:
-		players_by_id[player] = []
+		players_by_id[player] = database_pb2.players_entry()
 
 	# Look up all players in the database.
 	for key in players_by_id.keys():
@@ -68,10 +68,11 @@ def calc_leaders(database, leaderboards, output):
 		process = subprocess.Popen(run, stdout=subprocess.PIPE)
 		process.wait()
 
-		pb_player = database_pb2.players_entry()
-
-		pb_player.ParseFromString(process.stdout.read())
-		players_by_id[key] = pb_player
+		try:
+			players_by_id[key].ParseFromString(process.stdout.read())
+		except:
+			print 'Error getting player ' + str(key)
+			continue
 
 	# Now we have a list of all players. Time to make some HTML.
 
@@ -79,40 +80,58 @@ def calc_leaders(database, leaderboards, output):
 
 	html += "<div class='leaders'>"
 	html += "<h2>Daily Leaders</h2>"
-	html += "<table><tr><th>Player Name</th><th>Score</th></tr>"
+	html += "<table><col width='30'><tr><th></th><th>Player Name</th><th>Score</th></tr>\n"
+	rank = 1
 	for player_id in daily_leaders.leaders:
 		player = players_by_id[player_id]
-		profile_id = 76561197960265728 + player.account_id
-		unsafe_player_name = player.name
-		safe_player_name = cgi.escape( unsafe_player_name )
-		style = int(player.daily_style)
-		html += "<tr><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>"
+
+		if player.account_id == 0:
+			html += "<tr><td>" + str(rank) + "</td><td class='player'>Error</td><td></td></tr>"
+		else:
+			profile_id = 76561197960265728 + player.account_id
+			unsafe_player_name = player.name
+			safe_player_name = cgi.escape( unsafe_player_name )
+			style = int(player.daily_style)
+			html += "<tr class='rank" + str(rank) + " account" + str(player.account_id) + "'><td>" + str(rank) + "</td><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>\n"
+		rank += 1
 	html += "</table>"
 	html += "</div>"
 
 	html += "<div class='leaders'>"
 	html += "<h2>Weekly Leaders</h2>"
-	html += "<table><tr><th>Player Name</th><th>Score</th></tr>"
+	html += "<table><col width='30'><tr><th></th><th>Player Name</th><th>Score</th></tr>\n"
+	rank = 1
 	for player_id in weekly_leaders.leaders:
 		player = players_by_id[player_id]
-		profile_id = 76561197960265728 + player.account_id
-		unsafe_player_name = player.name
-		safe_player_name = cgi.escape( unsafe_player_name )
-		style = int(player.weekly_style)
-		html += "<tr><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>"
+
+		if player.account_id == 0:
+			html += "<tr><td>" + str(rank) + "</td><td class='player'>Error</td><td></td></tr>"
+		else:
+			profile_id = 76561197960265728 + player.account_id
+			unsafe_player_name = player.name
+			safe_player_name = cgi.escape( unsafe_player_name )
+			style = int(player.weekly_style)
+			html += "<tr class='rank" + str(rank) + " account" + str(player.account_id) + "'><td>" + str(rank) + "</td><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>\n"
+		rank += 1
 	html += "</table>"
 	html += "</div>"
 
 	html += "<div class='leaders'>"
 	html += "<h2>Monthly Leaders</h2>"
-	html += "<table><tr><th>Player Name</th><th>Score</th></tr>"
+	html += "<table><col width='30'><tr><th></th><th>Player Name</th><th>Score</th></tr>\n"
+	rank = 1
 	for player_id in monthly_leaders.leaders:
 		player = players_by_id[player_id]
-		profile_id = 76561197960265728 + player.account_id
-		unsafe_player_name = player.name
-		safe_player_name = cgi.escape( unsafe_player_name )
-		style = int(player.monthly_style)
-		html += "<tr><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>"
+
+		if player.account_id == 0:
+			html += "<tr><td>" + str(rank) + "</td><td class='player'>Error</td><td></td></tr>"
+		else:
+			profile_id = 76561197960265728 + player.account_id
+			unsafe_player_name = player.name
+			safe_player_name = cgi.escape( unsafe_player_name )
+			style = int(player.monthly_style)
+			html += "<tr class='rank" + str(rank) + " account" + str(player.account_id) + "'><td>" + str(rank) + "</td><td class='player'><a href='http://steamcommunity.com/profiles/" + str(profile_id) + "'>" + safe_player_name + "</a></td><td>" + str(style) + "</td></tr>\n"
+		rank += 1
 	html += "</table>"
 	html += "</div>"
 
